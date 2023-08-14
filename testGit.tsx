4226eb1711690d8705d8158971200e5d06c7d22b
BMasterton <99563726+BMasterton@users.noreply.github.com>
2023-08-14 15:05:32 -0700

    test 2 where i drag the opened file to the pre existing one im working on
    see title

--- testGit.tsx
+++ testGit.tsx
@@ -1,3 +1,4 @@
 // git commit check 
 // new desktop changes, for some reason its not always showing up 
 // this is a new test to see how the save is actually working with git repo
+// test number 2 baby 
